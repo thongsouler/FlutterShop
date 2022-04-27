ced3b7a1cc439e53749853d04360d8e6b2cac783
Hoàng Thông <46652538+thongsouler@users.noreply.github.com>
2022-04-27 13:26:38 +0700

    add dimissible cart

--- testnew/lib/widgets/cart_item_list.dart
+++ testnew/lib/widgets/cart_item_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:testnew/providers/cart.dart';
 
 class CartItemList extends StatelessWidget {
   final String id;
@@ -17,34 +19,45 @@ class CartItemList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final double totalAmount = price * quantity;
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      child: Padding(
-        padding: EdgeInsets.all(10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            CircleAvatar(
-              child: Icon(Icons.task),
-            ),
-            const SizedBox(
-              width: 10,
-            ),
-            Column(
-              children: [
-                Text(
-                  title,
-                  style: TextStyle(
-                      color: Theme.of(context).primaryColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16),
-                ),
-                Text('Total: ${totalAmount}')
-              ],
-            ),
-            Spacer(),
-            Text('$quantity x')
-          ],
+    return Dismissible(
+      key: ValueKey(productId),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: Icon(Icons.delete),
+      ),
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).remove(productId);
+      },
+      child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              CircleAvatar(
+                child: Icon(Icons.phone_in_talk),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Column(
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16),
+                  ),
+                  Text('Total: $totalAmount')
+                ],
+              ),
+              Spacer(),
+              Text('$quantity x')
+            ],
+          ),
         ),
       ),
     );
